0eee43589995430b8abe56510ea4b4db2bcff8a4
marilia <mariliarosilva@gmail.com>
2017-09-28 22:36:16 -0300

    backend login

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+
+/**
+ * Description of LoginController
+ *
+ * @author Marilia
+ */
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Usuarios;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class LoginController extends Controller {
+
+    public $formUserLogin;
+    public $error = null;
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request) {
+       if ($this->get('session')->get('idUser')) {
+
+            return $this->redirectToRoute('home');
+        } else {
+
+            $user = new Usuarios();
+
+            $this->formUserLogin = $this->createFormBuilder($user)
+                    ->add('emailusuario', TextType::class, array('label' => false))
+                    ->add('senhausuario', PasswordType::class, array('label' => false))
+                    ->getForm();
+
+            $this->formUserLogin->handleRequest($request);
+        }
+        if ($this->formUserLogin->isSubmitted() && $this->formUserLogin->isValid()) {
+            if ($this->autenticacao($user->getEmailusuario(), $user->getSenhausuario())) {
+                return $this->redirectToRoute('home');
+            } else {
+                return $this->render('login.html.twig', array(
+                            'form' => $this->formUserLogin->createView(), 'erro' => $this->error
+                ));
+            }
+        }
+        return $this->render('login.html.twig', array(
+                    'form' => $this->formUserLogin->createView(), 'erro' => $this->error
+        ));
+    
+    }
+
+    public function autenticacao($email, $senha) {
+
+       // $senha = hash('sha256', $senha);
+        $usuarioAutenticado = $this->getDoctrine()
+                ->getRepository('AppBundle:Usuarios')
+                ->findBy(array('emailusuario' => $email, 'senhausuario' => $senha));
+
+
+        if (!$usuarioAutenticado) {
+            $this->error = "Usuário ou senha inválidos!";
+            return false;
+        } else {
+           
+                $idUser = $usuarioAutenticado[0]->getIdusuario();
+                $this->get('session')->set('idUser', $idUser);
+
+                return true;
+           
+        }
+    }
+
+    /**
+     * @Route("/logout")
+     */
+    public function logoutAction() {
+
+        $this->get('session')->invalidate();
+        return $this->redirectToRoute('login');
+    }
+
+}
